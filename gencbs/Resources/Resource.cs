2e736b7bd5f71ca67a91851b01f2b0f2092a2d5e
waruna991 <warunashantha_991@yahoo.com>
2016-05-04 12:19:36 +0530

    add Resources package

---
+++ gencbs/Resources/Resource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gencbs.Resources
+{
+    class Resource
+    {
+        public ResourceType type;
+        public String name;
+        public int id;
+        public LinkedList<timeSlot> availability;
+
+        public int getCost()
+        {
+            int cost = 0;
+
+            return cost;        
+        }
+
+        public bool isAvailable(timeSlot timeToCheck)
+        {
+            bool isAvailable = false;
+
+
+            
+            return isAvailable;
+        }
+
+    }
+}
---
+++ gencbs/Resources/timeSlot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gencbs.Resources
+{
+    class timeSlot
+    {
+        public DateTime startTime;
+        public DateTime endTime;
+
+        public timeSlot(DateTime start ,DateTime end)
+        {
+            startTime = start;
+            endTime = end;
+        }
+    }
+}
